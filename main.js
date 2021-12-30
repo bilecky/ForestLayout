6bc07b34a48e0963eeb7a6b4ecb4c19283ae544b
Pawel <bilecky95@gmail.com>
2021-12-30 01:48:53 +0100

    rwd mobile friendly main site with ugly css

---
+++ main.js
@@ -0,0 +1,14 @@
+const navMobile = document.querySelector('.nav-mobile')
+
+const navBtn = document.querySelector('.hamburger')
+
+const handleNav = () => {
+
+    navBtn.classList.toggle('is-active')
+    navMobile.classList.toggle('nav-mobile--active')
+    navBtn.style.Color = "white"
+    }
+    
+    
+    navBtn.addEventListener('click', handleNav
+    )
