2bb80bb0b54f9c070db96e2918e4d908d61cae81
Pawel <bilecky95@gmail.com>
2021-12-30 18:44:43 +0100

    working desktop version

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const navMobile = document.querySelector('.nav-mobile')
 
 const navBtn = document.querySelector('.hamburger')
+const allNavItems = document.querySelectorAll('.nav__link')
 
 const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+            navBtn.classList.remove('is-active')
 
-    navBtn.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
-    navBtn.style.Color = "white"
-    }
-    
-    
-    navBtn.addEventListener('click', handleNav
-    )
+		})
+	})
+}
+
+navBtn.addEventListener('click', handleNav)
